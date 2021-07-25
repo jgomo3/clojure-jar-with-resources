01469ba04d81f40e95cbfd3db257046061933149
Jesús Gómez <jesus.gomez@fonemed.com>
2021-07-24 23:15:11 -0230

    Read a resource file to build the greeting message

--- src/jgomo3/hola.clj
+++ src/jgomo3/hola.clj
@@ -1,8 +1,13 @@
 (ns jgomo3.hola
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
 (defn greet [{:keys [who]}]
-  (println "Hello" (or who "world") "!"))
+  (let [greet-text (-> "hola.txt"
+                       io/resource
+                       io/file ;; could be skipped, but the code I'm having problems with does this.
+                       slurp)]
+    (println greet-text (or who "world") "!")))
 
 (defn -main [& args]
   (greet {:who (first args)}))
